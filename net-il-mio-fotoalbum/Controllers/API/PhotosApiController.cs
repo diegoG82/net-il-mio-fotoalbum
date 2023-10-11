2dc8b7c890a9134648231bbb0b24402f877a2527
diegoG82 <diegogiardina@hotmail.it>
2023-10-11 18:43:49 +0200

    Visualizzazione ericerca tramite api eseguita

---
+++ net-il-mio-fotoalbum/Controllers/API/PhotosApiController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.API
+
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PhotosApiController : ControllerBase
+
+    {
+        private PhotoContext _myDb;
+
+        public PhotosApiController(PhotoContext myDb)
+        {
+            _myDb = myDb;
+        }
+
+        //LISTA DI TUTTE LE FOTO
+        [HttpGet]
+        public IActionResult GetPhotos()
+        {
+
+            List<Photo> photos = _myDb.Photos.Include(p => p.categories).ToList();
+
+            if (photos.Count == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(photos);
+            }
+
+        }
+
+
+        //FILTRO CON TITOLO
+        [HttpGet]
+        public IActionResult GetPhotosByTitle(string? title)
+        {
+            if (title == null)
+            {
+                return GetPhotos();
+            }
+
+            List<Photo> photos = _myDb.Photos.Where(photo => photo.Title.Contains(title)).Include(photo => photo.categories).ToList();
+
+            if (photos.Count == 0)
+            {
+                return GetPhotos();
+            }
+            else
+            {
+                return Ok(photos);
+            }
+
+        }
+    }
+}
--- net-il-mio-fotoalbum/Program.cs
+++ net-il-mio-fotoalbum/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using net_il_mio_fotoalbum.Database;
+using System.Text.Json.Serialization;
+
 namespace net_il_mio_fotoalbum
 {
     public class Program
@@ -17,6 +19,11 @@ namespace net_il_mio_fotoalbum
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            // Codice di cofigurazione per il serializzatore JSON, in modo che ignori completamente le dipendenze cicliche di
+            // eventuali relazione N:N o 1:N presenti nel JSON risultante.
+            builder.Services.AddControllers().AddJsonOptions(x =>
+                            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
             var app = builder.Build();
 

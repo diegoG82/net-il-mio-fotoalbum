4ee9d37062eebd2dc0f3ea3b50f2298f996b60ee
diegoG82 <diegogiardina@hotmail.it>
2023-10-11 11:17:10 +0200

    Create classi photos e categories e dbcontext con identityuser

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+
+        //Many to many relation with Photo
+        public List<Photo> Photos { get; set; }
+
+        //Empty construct
+        public Category()
+        {
+
+        }
+    }
+
+
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Query;
+using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? Image { get; set; }
+
+        public bool? Visible { get; set; }
+
+
+        //Many to many relation with categories
+        public List<Category>? categories { get; set; }
+
+        //Empty construct
+        public Photo() { }
+
+
+        Photo (string title, string description, string image, bool visible)
+
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Image = image;
+            this.Visible = visible;
+        }
+    }
+}

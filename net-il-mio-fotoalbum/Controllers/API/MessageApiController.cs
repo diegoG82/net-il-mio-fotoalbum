3193a23272a9f8b1a5e26b9ce1577bb42e4bb1b9
diegoG82 <diegogiardina@hotmail.it>
2023-10-12 15:12:31 +0200

    Form Funzionante ed inizio sistemazione stile

---
+++ net-il-mio-fotoalbum/Controllers/API/MessageApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessageApiController : ControllerBase
+    {
+        private PhotoContext _myDb;
+
+        public MessageApiController(PhotoContext myDb)
+        {
+            _myDb = myDb;
+        }
+
+        [HttpPost]
+        public IActionResult StoreMessage([FromBody] Message message)
+        {
+            if (message == null)
+            {
+                return BadRequest("Dati non validi");
+            }
+
+            _myDb.Messages.Add(message);
+            _myDb.SaveChanges();
+
+            return Ok(new { success = true });
+        }
+
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Text { get; set; }
+
+        public Message(string email, string text)
+        {
+            this.Email = email;
+            this.Text = text;
+        }
+
+        public Message() { }
+    }
+
+
+}
